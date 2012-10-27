debb54f011b1651a08a63538c5998cb3a26bfc21
weixie <linegroup3@gmail.com>
2012-10-27 23:15:16 +0800

    add throw Exception

--- TS_PrepareData/src/linegroup3/tweetstream/preparedata/Main.java
+++ TS_PrepareData/src/linegroup3/tweetstream/preparedata/Main.java
@@ -17,7 +17,7 @@ public class Main {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception{
 		
 		//LoadTweetsInFile.statisticMonth();
 		
