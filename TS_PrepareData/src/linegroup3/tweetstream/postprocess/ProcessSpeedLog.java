3b8c5b3c77ce0a703e0e8a6bf6cae1f18d80248a
weixie <linegroup3@gmail.com>
2012-11-11 20:50:33 +0800

    special modification

--- TS_PrepareData/src/linegroup3/tweetstream/postprocess/ProcessSpeedLog.java
+++ TS_PrepareData/src/linegroup3/tweetstream/postprocess/ProcessSpeedLog.java
@@ -35,5 +35,32 @@ public class ProcessSpeedLog {
 		}
 		writer.close();
 	}
+	
+	public static void process2(String dir) throws Exception {
+		TreeMap<Timestamp, Pair> map = new TreeMap<Timestamp, Pair>();
+		
+		BufferedReader reader = new BufferedReader(new FileReader(dir + "/speedlog.txt"));
+		String line = null;
+		final int COUNT = 100000;
+		int cnt = 0;
+		while((line = reader.readLine()) != null && cnt < COUNT){
+			String[] res = line.split("\t");
+			Timestamp t = Timestamp.valueOf(res[0]);
+			Pair pair = new Pair(Double.parseDouble(res[1]), Double.parseDouble(res[2]));
+			map.put(t, pair);
+			cnt ++;
+		}		
+		
+		reader.close();
+		
+		BufferedWriter writer = new BufferedWriter(new FileWriter(dir + "/processedspeedlog.txt"));
+		for(Map.Entry<Timestamp, Pair> entry : map.entrySet()){
+			Timestamp t = entry.getKey();
+			Pair pair = entry.getValue();
+			
+			writer.write("" + t + "\t" + pair.v + "\t" + pair.a + "\n");
+		}
+		writer.close();
+	}
 		
 }
