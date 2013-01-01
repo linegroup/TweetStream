ab0553a3bc240b8934121c3187b2a9454341064c
Xie Wei <linegroup3@gmail.com>
2013-01-01 15:36:35 +0800

    add value term pair

---
+++ TS_PrepareData/src/linegroup3/tweetstream/postprocess/ValueTermPair.java
@@ -0,0 +1,10 @@
+package linegroup3.tweetstream.postprocess;
+
+public class ValueTermPair {
+	public double v = 0;
+	public String term = null;
+	public ValueTermPair(double v, String term){
+		this.v = v;
+		this.term = term;
+	}
+}
