ab4cf2ea9cb62966a2ee9739595a9e0cb2f2a176
Xie Wei <linegroup3@gmail.com>
2013-03-06 17:56:48 +0800

    change for hbase fetch api

--- TS_PrepareData/src/linegroup3/tweetstream/io/input/Fetcher.java
+++ TS_PrepareData/src/linegroup3/tweetstream/io/input/Fetcher.java
@@ -1,16 +1,58 @@
 package linegroup3.tweetstream.io.input;
 
-import java.util.ArrayList;
+import java.io.IOException;
+import java.util.LinkedList;
 import java.util.List;
 
+import org.json.JSONException;
 import org.json.JSONObject;
 
-import test.RealtimeAPITweetFetcher;
+import com.google.gson.Gson;
+
+import aek.hbasepoller.hbase.Tweet;
+import aek.hbasepoller.poller.HBaseTweetFetcher;
+
 
 public class Fetcher implements FetchTweets{
-
+	
+	private static HBaseTweetFetcher fetcher = null;
+	private static String hTableName = "plr_sg_tweet_live";
+	private static int lagMinute = 0;
+	private static int intervalMinute = 1;
+	
+	static{
+		try {
+			fetcher = new HBaseTweetFetcher(hTableName, lagMinute, intervalMinute);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
 	@Override
 	public List<JSONObject> fetch() {
+		try {
+			fetcher.fetch();
+			Gson gs = new Gson();
+			Tweet[] tweets = gs.fromJson(fetcher.getCurFetchJSONString(), Tweet[].class);
+			
+			List<JSONObject> ret = new LinkedList<JSONObject>();
+			for(Tweet tweet : tweets){
+				String str = gs.toJson(tweet);
+				JSONObject obj = new JSONObject(str);
+				ret.add(obj);
+			}
+			
+			return ret;
+			
+		} catch (IOException ie) {
+			ie.printStackTrace();
+		} catch (JSONException je) {
+			je.printStackTrace();
+		}
+		return null;
+	}
+	
+	/*public List<JSONObject> fetch() {
 		final String uri = "http://research.larc.smu.edu.sg:8080/PalanteerDevApi/rest/v1/tweets/search";
 		final String q = null;
 		final int delayMin = 1;
@@ -26,6 +68,6 @@ public class Fetcher implements FetchTweets{
 			e.printStackTrace();
 		}
 		return null; 
-	}
+	}*/
 
 }
