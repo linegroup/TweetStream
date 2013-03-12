ccc31e77c06c17965adc6ff94b71c3150d18d415
Xie Wei <linegroup3@gmail.com>
2013-03-12 19:52:15 +0800

    add function read tweets for FetcherMS

--- TS_PrepareData/src/linegroup3/tweetstream/io/input/FetcherMS.java
+++ TS_PrepareData/src/linegroup3/tweetstream/io/input/FetcherMS.java
@@ -15,8 +15,12 @@ import java.util.TimeZone;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import com.google.gson.Gson;
+
+import aek.hbasepoller.hbase.Tweet;
+
 //fetch tweets from mysql database 
-public class FetcherMS implements FetchTweets{ 
+public class FetcherMS implements FetchTweets, ReadTweets{ 
 
 	private Timestamp start = Timestamp.valueOf("2013-01-25 00:00:00");
 	private final Timestamp end = Timestamp.valueOf("2013-02-18 00:00:00");
@@ -84,6 +88,67 @@ public class FetcherMS implements FetchTweets{
 		return ret;
 	} 
 	
+	
+	@Override
+	public List<JSONObject> read(Timestamp ts, int minutes) {
+		Timestamp start = new Timestamp(ts.getTime() - minutes * 60 * 1000);
+		Timestamp end = new Timestamp(ts.getTime());
+		
+		List<JSONObject> ret = new LinkedList<JSONObject>();
+		Statement stmt = null;
+		ResultSet rs = null;
+		try {
+			stmt = conn.createStatement();
+			String sqlTxt = "select *  from newstream3 where t >= \'" + start + "\' and t < \'" + end +"\'" + " order by t";
+			if (stmt.execute(sqlTxt)) {
+				rs = stmt.getResultSet();
+				while (rs.next()) {
+					
+					String content = rs.getString("tweet");
+					if(content.startsWith("RT @")) continue;					
+					
+					long statusId = rs.getLong("status_ID");
+					long userId = rs.getLong("user_ID");
+					Timestamp t = rs.getTimestamp("t");
+					
+					
+					
+					Tweet tObj = new Tweet(statusId, userId, t.getTime(), content);
+					
+					ret.add(new JSONObject(new Gson().toJson(tObj)));
+				}
+			}
+		} catch (SQLException ex) {
+			// handle any errors
+			System.out.println("SQLException: " + ex.getMessage());
+			System.out.println("SQLState: " + ex.getSQLState());
+			System.out.println("VendorError: " + ex.getErrorCode());
+
+		} catch (JSONException e) {
+			e.printStackTrace();
+		} finally {
+			// it is a good idea to release
+			// resources in a finally{} block
+			// in reverse-order of their creation
+			// if they are no-longer needed
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException sqlEx) {
+				} // ignore
+				rs = null;
+			}
+			if (stmt != null) {
+				try {
+					stmt.close();
+				} catch (SQLException sqlEx) {
+				} // ignore
+				stmt = null;
+			}
+		}
+		return ret;
+	}
+	
 	static private Connection conn = null;
 	
 	static {
@@ -102,5 +167,7 @@ public class FetcherMS implements FetchTweets{
 
 		}
 	}
+
+
 
 }
