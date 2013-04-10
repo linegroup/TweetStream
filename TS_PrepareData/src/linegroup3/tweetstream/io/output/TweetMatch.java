e7e33ce5c546040690d74e3146b4fbf014fc9286
Xie Wei <linegroup3@gmail.com>
2013-04-10 20:21:29 +0800

    match event

--- TS_PrepareData/src/linegroup3/tweetstream/io/output/TweetMatch.java
+++ TS_PrepareData/src/linegroup3/tweetstream/io/output/TweetMatch.java
@@ -1,6 +1,9 @@
 package linegroup3.tweetstream.io.output;
 
+import linegroup3.tweetstream.event.OnlineEvent;
+
 
 public interface TweetMatch {
 	public boolean match(String tweet, String[] keywords);
+	public boolean match(String tweet, OnlineEvent event);
 }
