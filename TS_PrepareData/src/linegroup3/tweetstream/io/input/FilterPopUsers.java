4f05ddffde0b2fe89eaa0c77c13a4ceadeaccbf6
Xie Wei <linegroup3@gmail.com>
2013-04-10 20:14:57 +0800

    filter pop users

---
+++ TS_PrepareData/src/linegroup3/tweetstream/io/input/FilterPopUsers.java
@@ -0,0 +1,40 @@
+package linegroup3.tweetstream.io.input;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class FilterPopUsers implements FilterTweet{
+
+	@Override
+	public boolean filterOut(JSONObject tweet) {
+		String content;
+		try {
+			content = TweetExtractor.getContent(tweet);
+			return filterOut(content);
+		} catch (JSONException e) {
+			e.printStackTrace();
+			
+		}
+		return false;
+	}
+
+	@Override
+	public boolean filterOut(String content) {
+		String lowCase = content.toLowerCase();
+		
+		if(lowCase.contains("@kingsleyyy")) 
+			return true;
+		if(lowCase.contains("@girlposts")) 
+			return true;
+		if(lowCase.contains("@freddyamazin")) 
+			return true;
+		if(lowCase.contains("@comedyposts")) 
+			return true;
+		if(lowCase.contains("@wethinkforgirls")) 
+			return true;
+		if(lowCase.contains("@girlnotes")) 
+			return true;
+		return false;
+	}
+
+}
