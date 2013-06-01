e569b4c7f0c7565c3f0401e49485a2a4d58f3f7d
xiewei <xiewei@INFINITY>
2013-06-01 15:31:57 +0800

    add config

--- TS_PrepareData/src/linegroup3/common/Config.java
+++ TS_PrepareData/src/linegroup3/common/Config.java
@@ -1,5 +1,7 @@
 package linegroup3.common;
 
+import java.sql.Timestamp;
+
 import org.apache.commons.configuration.Configuration;
 import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.configuration.PropertiesConfiguration;
@@ -19,4 +21,15 @@ public class Config {
 	}
 	
 	static public int N = 300;
+	
+	/////////// FetcherMS
+	static public Timestamp FetcherMS_start = Timestamp.valueOf("2010-06-02 00:00:00");
+	static public Timestamp FetcherMS_end = Timestamp.valueOf("2010-06-13 00:00:00");
+	static public String FetcherMS_db = "tweetstream";
+	static public String FetcherMS_table = "stream_2010_06";
+	
+	/////////// Detection time
+	static public Timestamp detectionT = Timestamp.valueOf("2010-06-07 00:00:00");
+	
+	
 }
